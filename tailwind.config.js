cc868196c3e92c6d92634f03ef39cf949fc22a72
Heitor Zelbato <heitorzelbato@gmail.com>
2025-07-02 15:41:09 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.php",
+    "./src/**/*.scss",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
